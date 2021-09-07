37e4a844d64daf275a64d018b13f8120d0aaefe2
Rhea Dsouza <rheadqs@gmail.com>
2021-09-07 15:06:36 +0530

    Variables Added

--- src/JpLab.java
+++ src/JpLab.java
@@ -1,3 +1,18 @@
-public class JpLab {
+/*
+         This is my Java Program
+  */
+class Example{
+    public static void main(String args[])
+    {
+        int num = 10;
+        System.out.println("Num = "+num);
+        num = 2 * num;
+        System.out.println("Value of num is multiplied by 2")
+        System.out.println(num);
+    }
 
 }
+
+
+
+
